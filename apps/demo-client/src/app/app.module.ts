2d727f58a9e28cfb65c5d9c019367f903831d4f6
Michael Berger <m.gonser@icloud.com>
2022-08-08 09:28:53 +0200

    example with a lot of unused modules

--- apps/demo-client/src/app/app.module.ts
+++ apps/demo-client/src/app/app.module.ts
@@ -4,12 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AllModulesModule} from "@treeshaking-material-demo/all-modules";
 
 
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [BrowserModule, BrowserAnimationsModule, AllModulesModule],
   providers: [],
   bootstrap: [AppComponent],
 })
