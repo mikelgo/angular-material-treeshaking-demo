43a3dd4b7da8e8a68387cb8ce57abb3c3b6a6cfb
Michael Berger <m.gonser@icloud.com>
2022-08-08 09:01:43 +0200

    initial commit

---
+++ apps/demo-client/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'treeshaking-material-demo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'demo-client';
+}
