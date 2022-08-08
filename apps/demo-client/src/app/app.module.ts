3d74892894818045d943bf88eae9138a41e90db1
Michael Berger <m.gonser@icloud.com>
2022-08-08 09:28:05 +0200

    example with only needed modules

--- apps/demo-client/src/app/app.module.ts
+++ apps/demo-client/src/app/app.module.ts
@@ -4,12 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {ScamModulesModule} from "@treeshaking-material-demo/scam-modules";
 
 
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [BrowserModule, BrowserAnimationsModule, ScamModulesModule],
   providers: [],
   bootstrap: [AppComponent],
 })
