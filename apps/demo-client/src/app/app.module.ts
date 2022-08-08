5ede83bc884d5e1cab40cc81afa9e480ec77ad74
Michael Berger <m.gonser@icloud.com>
2022-08-08 11:38:28 +0200

    fix example

--- apps/demo-client/src/app/app.module.ts
+++ apps/demo-client/src/app/app.module.ts
@@ -4,13 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {AllModulesModule} from "@treeshaking-material-demo/all-modules";
+
 
 
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, AllModulesModule],
+  imports: [BrowserModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
